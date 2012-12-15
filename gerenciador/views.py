4f1e2fd267bac26b3b6c5b7431555eb2b4162c28
Wellington Silva <wsilva@usp.br>
2012-12-15 17:10:16 -0200

    Adicionando view para mostrar os registros no template de listagem

--- gerenciador/views.py
+++ gerenciador/views.py
@@ -2,5 +2,11 @@
 
 from django.http import HttpResponse
 
+from models import RegistroPonto
+
 def home(request):
     return HttpResponse(u"Esta é a página inicial!")
+
+def lista(request):
+    lista_registros = RegistroPonto.objects.all()
+    return render_to_response("lista.html", {'lista_registros':lista_registros})
