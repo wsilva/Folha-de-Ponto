c7f7a7a20d7bc5e950664266260dc97974c54491
Wellington Silva <wsilva@usp.br>
2012-12-16 15:38:51 -0200

    vinculando os registros a usuário no admin (crud completo)

--- gerenciador/admin.py
+++ gerenciador/admin.py
@@ -10,4 +10,14 @@ class RegistroPontoAdmin(admin.ModelAdmin):
     fields = ('dia', 'hora', 'tipo', 'imagem')
     list_display = ('dia', 'hora', 'tipo')
 
+    def save_model(self, request, obj, form, change):
+        obj.usuario = request.user
+        obj.save()
+
+    def queryset(self,request):
+        qs = super(RegistroPontoAdmin, self).queryset(request)
+        return qs.filter(usuario = request.user)
+
+
+
 admin.site.register(RegistroPonto, RegistroPontoAdmin)
