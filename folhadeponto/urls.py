55ac70ee4927e5f0d4c9f031b885914865895da8
Wellington Silva <wsilva@usp.br>
2012-12-16 09:07:38 -0200

    adicionando icone de remover, acertando a rota e alterando o template

--- folhadeponto/urls.py
+++ folhadeponto/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -10,6 +11,7 @@ urlpatterns = patterns('',
     url(r'^registros/$', 'gerenciador.views.lista', name='lista'),
     url(r'^novoregistro/$', 'gerenciador.views.novo', name='novo'),
     url(r'^registro/(?P<id_registro>\d+)/$', 'gerenciador.views.editar', name='editar'),
+    url(r'^media/(.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
     # url(r'^folhadeponto/', include('folhadeponto.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
@@ -18,3 +20,8 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        url(r'^media/(?<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+    )
