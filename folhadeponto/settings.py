db288974a8f572e22bf484a5902a3815ef03131d
Wellington Silva <wellington.silva@dafiti.com.br>
2013-01-17 10:23:20 -0200

    mudando o banco de dados para arquivo (sqlite)

--- folhadeponto/settings.py
+++ folhadeponto/settings.py
@@ -14,12 +14,12 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'folhadeponto',                      # Or path to database file if using sqlite3.
-        'USER': 'root',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'folhadeponto.sql',                      # Or path to database file if using sqlite3.
+        # 'USER': 'root',                      # Not used with sqlite3.
+        # 'PASSWORD': 'dafiti',                  # Not used with sqlite3.
+        # 'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        # 'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
 
